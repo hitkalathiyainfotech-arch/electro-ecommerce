c8e604a9e4eb377df19865ec9b668729731ddcbb
DarshanBackend <darshan1.kalathiyainfotech@gmail.com>
2026-03-20 12:57:09 +0530

    20-03-2026

--- config/email.config.js
+++ config/email.config.js
@@ -5,7 +5,9 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
-  requireTLS: true,
+  tls: {
+    rejectUnauthorized: false
+  },
   auth: {
     user: process.env.EMAIL_USER || "hit.kalathiyainfotech@gmail.com",
     pass: process.env.EMAIL_PASS || "fxea ykui olbg oqoh",
--- utils/Email.config.js
+++ utils/Email.config.js
@@ -6,7 +6,9 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
-  requireTLS: true,
+  tls: {
+    rejectUnauthorized: false
+  },
   auth: {
     user: process.env.EMAIL_USER || "darshan1.kalathiyainfotech@gmail.com",
     pass: process.env.EMAIL_PASS || "nxjtawvfcfmwmhjp",
